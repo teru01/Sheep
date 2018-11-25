2d55748bc5005ff92c33b5cde973a549413f4f73
teruya <abas.trace.on@gmail.com>
2018-11-25 20:37:40 +0900

    feat: add Symbols for fields

---
+++ src/sheep/chap12/MemberSymbols.java
@@ -0,0 +1,35 @@
+package sheep.chap12;
+import sheep.chap11.*;
+
+public class MemberSymbols extends Symbols {
+    public static int METHOD = -1;
+    public static int FIELD = -2;
+
+    protected int type;
+    public MemberSymbols(Symbols outer, int type) {
+        super(outer);
+        this.type = type;
+    }
+
+    @Override
+    public Location get(String key, int nest) {
+        Integer index = this.table.get(key);
+        if(index != null) {
+            return new Location(type, index.intValue());
+        }
+        if(this.outer != null) {
+            return this.outer.get(key, nest);
+        }
+        return null;
+    }
+
+    @Override
+    public Location put(String key) {
+        Location loc = this.get(key, 0);
+        if(loc == null) {
+            return new Location(this.type, this.add(key));
+        } else {
+            return loc;
+        }
+    }
+}
---
+++ src/sheep/chap12/SymbolThis.java
@@ -0,0 +1,27 @@
+package sheep.chap12;
+import sheep.*;
+import sheep.chap11.*;
+
+public class SymbolThis extends Symbols {
+    //thisは局所変数と解釈される
+    public static final String NAME = "this";
+
+    public SymbolThis(Symbols outer) {
+        super(outer);
+        this.add(NAME);
+    }
+
+    @Override
+    public int putNew(String key) {
+        throw new SheepException("fatal error. cannot use String for putNew()");
+    }
+
+    @Override
+    public Location put(String key) {
+        Location loc = this.outer.put(key);
+        if(loc.nest >= 0) {
+            loc.nest++;
+        }
+        return loc;
+    }
+}
