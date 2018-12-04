01a6a279bd86de295e19348427b50f74059138e4
teruya <abas.trace.on@gmail.com>
2018-12-04 12:05:18 +0900

    feat: add foriterexpr

---
+++ src/sheep/ast/ForIterExpr.java
@@ -0,0 +1,36 @@
+package sheep.ast;
+import java.util.*;
+
+public class ForIterExpr extends ASTList{
+    public ForIterExpr(List<ASTree> c) {
+        super(c);
+    }
+
+    /**
+     * forの初期化式
+     */
+    public ASTree initExpr() {
+        return this.child(0);
+    }
+
+    /**
+     * for条件式
+     * @return
+     */
+    public ASTree contitionExpr() {
+        return this.child(1);
+    }
+
+    /**
+     * for更新式
+     * @return
+     */
+    public ASTree updateExpr() {
+        return this.child(2);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + initExpr() + "," + contitionExpr() + "," + updateExpr() + ")";
+    }
+}
