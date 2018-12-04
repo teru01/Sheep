1328d58f2e9f00369fa453e5956d8cc4a1562021
teruya <abas.trace.on@gmail.com>
2018-12-04 12:04:58 +0900

    feat: add forparser

---
+++ src/sheep/ForParser.java
@@ -0,0 +1,16 @@
+package sheep;
+import javassist.gluonj.Reviser;
+import sheep.ast.*;
+import static sheep.Parser.rule;
+
+@Reviser
+class ForParser extends BasicParser {
+    Parser iterControl = rule(ForIterExpr.class)
+                            .ast(this.factor)
+                            .ast(this.factor)
+                            .ast(this.factor);
+    Parser sheepfor = rule(ForStmnt.class)
+                        .sep("for")
+                        .ast(this.iterControl)
+                        .ast(this.block);
+}
