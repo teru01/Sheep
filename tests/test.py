ae1b43c56fd85bbba09331784012578957365199
teruya <abas.trace.on@gmail.com>
2018-12-07 17:19:47 +0900

    test: add ary test case

--- tests/test.py
+++ tests/test.py
@@ -24,6 +24,7 @@ testfiles = [
     "tests/closure_test.sheep",
     "tests/for_test.sheep",
     "tests/const_test.sheep",
+    "tests/ary_test.sheep",
 ]
 answer = [
     "21\n2\n9\n32\n",
@@ -33,6 +34,7 @@ answer = [
     "5\n3\n8\n",
     "0\n1\n2\n0\n1\n1\n1\n10\n",
     "20\n2\n11\n",
+    "2\nhoge\nbar\nteru\n"
 ]
 
 
