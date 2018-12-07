6a690448eb20ba05cbab675d404eb5c3a80dbd01
teruya <abas.trace.on@gmail.com>
2018-12-07 14:18:13 +0900

    feat:add const new files

---
+++ src/sheep/ast/ConstExpr.java
@@ -0,0 +1,21 @@
+package sheep.ast;
+
+import java.util.*;
+
+public class ConstExpr extends ASTList {
+    public ConstExpr(List<ASTree> c) {
+        super(c);
+    }
+
+    /**
+     * const shep = 10 と言う文のshepを取得する
+     */
+    public ASTree getConstant() {
+        return this.child(0);
+    }
+
+    @Override
+    public String toString() {
+        return "const " + this.getConstant();
+    }
+}
---
+++ src/sheep/extra/ConstEvaluator.java
@@ -0,0 +1,52 @@
+package sheep.extra;
+
+import java.util.List;
+
+import javassist.gluonj.Require;
+import javassist.gluonj.Reviser;
+import sheep.ConstParser;
+import sheep.SheepConstException;
+import sheep.SheepException;
+import sheep.ast.ASTree;
+import sheep.ast.ConstExpr;
+import sheep.ast.Name;
+import sheep.chap6.BasicEvaluator.BinaryEx;
+import sheep.chap6.Environment;
+import sheep.chap7.ClosureEvaluator;
+import sheep.chap7.FuncEvaluator.EnvEx;
+
+@Require({ClosureEvaluator.class, ConstParser.class})
+@Reviser
+public class ConstEvaluator {
+    @Reviser
+    public static class ConstExprEx extends ConstExpr {
+        public ConstExprEx(List<ASTree> c) {
+            super(c);
+        }
+
+        public Object eval(Environment evn) {
+            throw new SheepException("const variable must have an initializer", this);
+        }
+    }
+
+    @Reviser
+    public static class AssignExForConst extends BinaryEx {
+        public AssignExForConst(List<ASTree> c) {
+            super(c);
+        }
+
+        @Override
+        protected Object computeAssign(Environment env, Object right) {
+            ASTree left = this.left();
+            if (this.left() instanceof ConstExpr) {
+                ConstExpr leftConst = (ConstExpr) left;
+                if (!(leftConst.getConstant() instanceof Name)) {
+                    throw new SheepException("bad assignment", this);
+                }
+                ((EnvEx) env).putConst(((Name) leftConst.getConstant()).name(), right);
+                return right;
+            }
+            return super.computeAssign(env, right);
+        }
+    }
+}
